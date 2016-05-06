09d08764a1412ce44778b95032411b0329741c2d
Mike Bostock <mbostock@gmail.com>
2016-05-06 11:05:26 -0700

    Slightly cleaner separation of concerns.

--- src/drag.js
+++ src/drag.js
@@ -2,8 +2,6 @@ import constant from "./constant";
 import noop from "./noop";
 import {event, select, mouse, touch} from "d3-selection";
 
-var map = Array.prototype.map;
-
 function nodefault() {
   event.preventDefault();
 }
@@ -18,14 +16,6 @@ function defaultContainer() {
   return this.parentNode;
 }
 
-function mouseId() {
-  return [null];
-}
-
-function touchId() {
-  return map.call(event.changedTouches, function(t) { return t.identifier; });
-}
-
 // Listen to the window for mousemove and mouseup such that we continue to
 // receive events if the mouse goes outside the window.
 function mouseContext() {
@@ -38,10 +28,10 @@ function touchContext() {
 }
 
 export default function(dragstarted) {
-  var container = defaultContainer,
-      filter = defaultFilter,
-      mousedowned = started("mousemove", "mouseup", mouse, mouseId, mouseContext),
-      touchstarted = started("touchmove", "touchend touchcancel", touch, touchId, touchContext);
+  var filter = defaultFilter,
+      container = defaultContainer,
+      mousestart = start("mousemove", "mouseup", mouse, mouseContext),
+      touchstart = start("touchmove", "touchend touchcancel", touch, touchContext);
 
   function drag(selection) {
     selection
@@ -49,65 +39,76 @@ export default function(dragstarted) {
         .on("touchstart.drag", touchstarted);
   }
 
-  function started(move, end, position, identify, contextify) {
-    return function() {
-      var that = this;
-      identify().filter(filter, that).forEach(function(id) {
-        var node = container.call(that, id),
-            p0 = position(node, id),
-            listen = dragstarted.call(that, p0[0], p0[1], id) || noop,
-            dragged = typeof listen === "function" ? listen : listen.drag || noop,
-            dragended = listen.dragend || noop,
-            noclick = false,
-            view = select(event.view).on(name("dragstart selectstart"), nodefault, true),
-            context = select(contextify()).on(name(move), moved).on(name(end), ended);
+  function mousedowned() {
+    if (filter.call(this)) {
+      mousestart.call(this);
+    }
+  }
 
-        // I’d like to call preventDefault on mousedown to disable native dragging
-        // of links or images and native text selection. However, in Chrome this
-        // causes mousemove and mouseup events outside an iframe to be dropped:
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=269917
-        //
-        // And if you preventDefault on touchstart on iOS, it prevents the click
-        // event on touchend, even if there was no touchmove! So instead, we
-        // cancel the specific undesirable behaviors.
+  function touchstarted() {
+    for (var touches = event.changedTouches, i = 0, n = touches.length, id; i < n; ++i) {
+      if (filter.call(this, id = touches[i].identifier)) {
+        touchstart.call(this, id);
+      }
+    }
+  }
 
-        function name(types) {
-          var name = id == null ? ".drag" : ".drag-" + id;
-          return types == null ? name : types.trim()
-              .split(/^|\s+/)
-              .map(function(type) { return type + name; })
-              .join(" ");
-        }
+  function start(move, end, position, contextify) {
+    return function(id) {
+      var node = this,
+          parent = container.call(node, id),
+          p0 = position(parent, id),
+          listen = dragstarted.call(node, p0[0], p0[1], id) || noop,
+          dragged = typeof listen === "function" ? listen : listen.drag || noop,
+          dragended = listen.dragend || noop,
+          noclick = false,
+          view = select(event.view).on(name("dragstart selectstart"), nodefault, true),
+          context = select(contextify()).on(name(move), moved).on(name(end), ended);
 
-        function moved() {
-          var p = position(node, id);
-          if (p == null) return; // This touch didn’t change.
-          nodefault(), noclick = true;
-          dragged.call(that, p[0], p[1], id);
-        }
+      // I’d like to call preventDefault on mousedown to disable native dragging
+      // of links or images and native text selection. However, in Chrome this
+      // causes mousemove and mouseup events outside an iframe to be dropped:
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=269917
+      // And if you preventDefault on touchstart on iOS, it prevents the click
+      // event on touchend, even if there was no touchmove! So instead, we
+      // cancel the specific undesirable behaviors.
 
-        function ended() {
-          var p = position(node, id);
-          if (p == null) return; // This touch didn’t end.
-          view.on(name(), null);
-          context.on(name(), null);
-          if (noclick) view.on(name("click"), nodefault, true), setTimeout(afterended, 0);
-          dragended.call(that, p[0], p[1], id);
-        }
+      function name(types) {
+        var name = id == null ? ".drag" : ".drag-" + id;
+        return types == null ? name : types.trim()
+            .split(/^|\s+/)
+            .map(function(type) { return type + name; })
+            .join(" ");
+      }
 
-        function afterended() {
-          view.on(name("click"), null);
-        }
-      });
+      function moved() {
+        var p = position(parent, id);
+        if (p == null) return; // This touch didn’t change.
+        nodefault(), noclick = true;
+        dragged.call(node, p[0], p[1], id);
+      }
+
+      function ended() {
+        var p = position(parent, id);
+        if (p == null) return; // This touch didn’t end.
+        view.on(name(), null);
+        context.on(name(), null);
+        if (noclick) view.on(name("click"), nodefault, true), setTimeout(afterended, 0);
+        dragended.call(node, p[0], p[1], id);
+      }
+
+      function afterended() {
+        view.on(name("click"), null);
+      }
     };
   }
 
-  drag.container = function(_) {
-    return arguments.length ? (container = typeof _ === "function" ? _ : constant(_), drag) : container;
-  };
-
   drag.filter = function(_) {
     return arguments.length ? (filter = typeof _ === "function" ? _ : constant(!!_), drag) : filter;
+  };
+
+  drag.container = function(_) {
+    return arguments.length ? (container = typeof _ === "function" ? _ : constant(_), drag) : container;
   };
 
   return drag;
