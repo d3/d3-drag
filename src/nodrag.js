fecdfc709313f5dffca60e56668567d2e16470e4
Mike Bostock <mbostock@gmail.com>
2016-05-09 10:38:47 -0700

    Fix #1 - support -moz-user-select.

--- src/nodrag.js
+++ src/nodrag.js
@@ -2,9 +2,9 @@ import {event, select} from "d3-selection";
 import cancel from "./cancel";
 
 export default function() {
-  select(event.on("end.nodrag", yesdrag).sourceEvent.view).on("dragstart.nodrag-" + event.identifier, cancel, true);
-}
-
-function yesdrag() {
-  select(event.sourceEvent.view).on("dragstart.nodrag-" + event.identifier, null);
+  var dragstart = "dragstart.nodrag-" + event.identifier,
+      view = select(event.sourceEvent.view).on(dragstart, cancel, true);
+  event.on("end.nodrag", function() {
+    view.on(dragstart, null);
+  });
 }
--- src/noselect.js
+++ src/noselect.js
@@ -1,10 +1,23 @@
 import {event, select} from "d3-selection";
 import cancel from "./cancel";
 
-export default function() {
-  select(event.on("end.noselect", yesselect).sourceEvent.view).on("selectstart.noselect-" + event.identifier, cancel, true);
+function noselectstart() {
+  var selectstart = "selectstart.noselect-" + event.identifier,
+      view = select(event.sourceEvent.view).on(selectstart, cancel, true);
+  event.on("end.noselect", function() {
+    view.on(selectstart, null);
+  });
 }
 
-function yesselect() {
-  select(event.sourceEvent.view).on("selectstart.noselect-" + event.identifier, null);
+function nouserselect() {
+  var style = this.ownerDocument.documentElement.style,
+      value = style.MozUserSelect;
+  style.MozUserSelect = "none";
+  event.on("end.noselect", function() {
+    style.MozUserSelect = value;
+  });
+}
+
+export default function() {
+  return ("onselectstart" in this ? noselectstart : nouserselect).apply(this, arguments);
 }
